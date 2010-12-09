a501664926bbd863353ef278c113cbd6e29c8bbc
unknown <SSingh@.Enterprise.NBTY.Global>
2010-12-09 11:23:03 -0500

    updated

--- product/nothinbutdotnetstore.specs/web/ViewMainDepartmentsInTheStoreSpecs.cs
+++ product/nothinbutdotnetstore.specs/web/ViewMainDepartmentsInTheStoreSpecs.cs
@@ -17,8 +17,32 @@ namespace nothinbutdotnetstore.specs.web
         [Subject(typeof(ViewMainDepartmentsInTheStore))]
         public class when_observation_name : concern
         {
-        
-                
+
+            Establish c = () =>
+            {
+                request = an<Request>();
+                provide_a_basic_sut_constructor_argument<RequestSpecification>(x =>
+                {
+                    deps = new IList<RequestDepartments>();
+                    return IList<RequestDepartments>;
+                });
+
+
+
+                var depatmemts = new IList<RequestDepartments>();
+            };
+            Because b = () =>
+                result = sut.process(request);
+
+            It should_make_the_determination_by_using_its_request_specification = () =>
+            {
+                result.ShouldBeTrue();
+                was_called.ShouldBeTrue();
+            };
+
+            static Request request;
+            static bool result;
+            private static IList<RequestDepartments> deps;
         }
     }
 }
--- product/nothinbutdotnetstore/web/infrastructure/RequestSpecification.cs
+++ product/nothinbutdotnetstore/web/infrastructure/RequestSpecification.cs
@@ -2,4 +2,7 @@ namespace nothinbutdotnetstore.web.infrastructure
 {
     public delegate bool RequestSpecification(Request request);
 
+    public delegate IList<RequestDepartments> GetDepatmentList(Request request );
+
 }
+
